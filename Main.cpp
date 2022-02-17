e0804e66a2d7d4ea59d2b1bab7c243ee8e7603c9
Mr-Gradus <rybin.dmitriy@gmail.com>
2022-02-18 00:30:34 +0500

    Very hard ))

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "Project1.h"
+#include "Test.h"
+
+
+
+int main()
+{
+	
+	
+		
+	
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+
+
+	return 0;
+}
